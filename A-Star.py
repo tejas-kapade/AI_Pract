035305e5257126a215c5bd55bfcff6e72c32a34a
Tejas Kapade <75604489+tejas-kapade@users.noreply.github.com>
2022-10-17 21:21:09 +0530

    A-Star.py
    A* algorithm for AI Practical

---
+++ A-Star.py
@@ -0,0 +1,47 @@
+#501(prac3)------27/06/19------
+#AIM: Implement A* search.
+
+import queue as Q
+from RMP import dict_gn
+from RMP import dict_hn
+
+start='Arad'
+goal='Bucharest'
+result=''
+
+def get_fn(citystr):
+    cities=citystr.split(" , ")
+    hn=gn=0
+    for ctr in range(0, len(cities)-1):
+        gn=gn+dict_gn[cities[ctr]][cities[ctr+1]]
+    hn=dict_hn[cities[len(cities)-1]]
+    return(hn+gn)
+
+def expand(cityq):
+    global result
+    tot, citystr, thiscity=cityq.get()
+    if thiscity==goal:
+        result=citystr+" : : "+str(tot)
+        return
+    for cty in dict_gn[thiscity]:
+        cityq.put((get_fn(citystr+" , "+cty), citystr+" , "+cty, cty))
+    expand(cityq)
+
+def main():
+    cityq=Q.PriorityQueue()
+    thiscity=start
+    cityq.put((get_fn(start),start,thiscity))
+    expand(cityq)
+    print("The A* path with the total is: ")
+    print(result)
+
+main()
+
+"""
+OUTPUT:
+The A* path with the total is: 
+Arad , Sibiu , Rimnicu , Pitesti , Bucharest : : 418
+"""
+
+        
+        
